501d7bdf3f9c749c608d51e3ae372095a8f43a72
Gumas <148417921+Opmasterod@users.noreply.github.com>
2024-11-30 10:31:29 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -127,15 +127,16 @@ async def start_command(client: Client, message: Message):
                 except:
                     pass
 
-            SD = await message.reply_text(f"<b>‼️ Watch Fast Lectures and Notes before Deleted after {get_exp_time(SECONDS)}.\n\nIf Your Lecture Pdf Deleted Don't worry you again able to access 🥰\n\n Go back from where you got link and again click on link and get Again\n\n𝐒𝐨𝐫𝐫𝐲,𝐅𝐨𝐫 𝐭𝐡𝐢𝐬 𝐍𝐨𝐭 𝐅𝐨𝐫𝐰𝐚𝐫𝐝𝐢𝐧𝐠 𝐨𝐧 𝐚𝐧𝐝 𝐧𝐨𝐭 𝐟𝐨𝐫 𝐚 𝐟𝐮𝐥𝐥 𝐭𝐢𝐦𝐞 𝐛𝐜𝐳 𝐰𝐞 𝐠𝐨𝐭 𝐜𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭𝐬 😖😖 🙏</b>")
-            await asyncio.sleep(SECONDS)
-
-            for snt_msg in snt_msgs:
-                try:
-                    await snt_msg.delete()
-                    await SD.delete()
-                except:
-                    pass
+            if SECONDS != 0:
+                notification_msg = await message.reply(f"<b>‼️ Watch Fast Lectures and Notes before Deleted after {get_exp_time(SECONDS)}.\n\nIf Your Lecture Pdf Deleted Don't worry you again able to access 🥰\n\n Go back from where you got link and again click on link and get Again\n\n𝐒𝐨𝐫𝐫𝐲,𝐅𝐨𝐫 𝐭𝐡𝐢𝐬 𝐍𝐨𝐭 𝐅𝐨𝐫𝐰𝐚𝐫𝐝𝐢𝐧𝐠 𝐨𝐧 𝐚𝐧𝐝 𝐧𝐨𝐭 𝐟𝐨𝐫 𝐚 𝐟𝐮𝐥𝐥 𝐭𝐢𝐦𝐞 𝐛𝐜𝐳 𝐰𝐞 𝐠𝐨𝐭 𝐜𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭𝐬 😖😖 🙏</b>")
+                await asyncio.sleep(SECONDS)
+                for snt_msg in snt_msgs:
+                    try:
+                        await snt_msg.delete()
+                    except:
+                        pass
+                await notification_msg.delete()
+                return
 
         elif verify_status['is_verified']:
             reply_markup = InlineKeyboardMarkup(
